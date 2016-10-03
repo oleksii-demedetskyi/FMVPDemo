3c879cd1e9f54d83cf4fd0a0012899fa95d4a6cc
Alexey Demedetskiy <alexey.demedetskiy@sigma.software>
2016-10-03 23:40:25 +0200

    Move UI to Main storyboard

---
+++ FMVPDemo/LoginViewController.swift
@@ -0,0 +1,16 @@
+//
+//  LoginViewController.swift
+//  FMVPDemo
+//
+//  Created by Alexey Demedetskii on 10/3/16.
+//  Copyright © 2016 dalog. All rights reserved.
+//
+
+import UIKit
+
+final class LoginViewController: UIViewController {
+    @IBOutlet private var usernameField: UITextField!
+    @IBOutlet private var passwordField: UITextField!
+    @IBOutlet private var loginButton: UIButton!
+    @IBOutlet private var activityIndicator: UIActivityIndicatorView!
+}
