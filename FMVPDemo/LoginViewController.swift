12fbb908c95c02625fff28a9650bed65409d47b4
Alexey Demedetskiy <alexey.demedetskiy@sigma.software>
2016-10-04 01:22:08 +0200

    Add login pseudocode

--- FMVPDemo/LoginViewController.swift
+++ FMVPDemo/LoginViewController.swift
@@ -13,4 +13,37 @@ final class LoginViewController: UIViewController {
     @IBOutlet private var passwordField: UITextField!
     @IBOutlet private var loginButton: UIButton!
     @IBOutlet private var activityIndicator: UIActivityIndicatorView!
+    
+    @IBAction func login() {
+        let username = usernameField.text
+        let password = passwordField.text
+        
+        view.endEditing(true)
+        activityIndicator.startAnimating()
+        self.loginButton.isEnabled = false
+        DispatchQueue.global(qos: .background).asyncAfter(deadline: DispatchTime.now() + .seconds(3)) {
+            // WARNING: emulation of server side code
+            if username == "mobiconf" && password == "rocks!" {
+                // Handle success
+                DispatchQueue.main.async {
+                    self.activityIndicator.stopAnimating()
+                    self.loginButton.isEnabled = true
+                    self.performSegue(withIdentifier: "show timers", sender: self)
+                }
+            } else {
+                // Handle fail
+                DispatchQueue.main.async {
+                    self.activityIndicator.stopAnimating()
+                    self.loginButton.isEnabled = true
+                    let alert = UIAlertController(
+                        title: "Incorrect user",
+                        message: "User is incorrect. Please login again",
+                        preferredStyle: .alert)
+                    alert.addAction(.init(title: "Ok", style: .cancel, handler: nil))
+                    self.present(alert, animated: true, completion: nil)
+                }
+            }
+            
+        }
+    }
 }
